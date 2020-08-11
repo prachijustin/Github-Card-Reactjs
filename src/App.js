79ac7de10beaad08ceb4470aa73bdd749964ea26
prachijustin <prachi.sharma01sep@gmail.com>
2020-08-11 16:10:32 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import "./App.css";
 import CardList from "./components/CardList";
 import Form from "./components/Form";
 
-// const testData = [
-//   {
-//     name: "Dan Abramov",
-//     avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
-//     company: "@facebook",
-//   },
-//   {
-//     name: "Sophie Alpert",
-//     avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
-//     company: "Humu",
-//   },
-//   {
-//     name: "Sebastian Markbåge",
-//     avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
-//     company: "Facebook",
-//   },
-//];
-
 class App extends Component {
   state = {
     profiles: [],
